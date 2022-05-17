4d817d00d42dd5bf7d5a5939afa929f644494290
2030lab <bob.score@2030lab.com>
2022-05-17 13:45:03 +0100

    end of the basics

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -4,15 +4,41 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
   //selector: '[app-servers]',
   //selector: '.app-servers',
-  //templateUrl: './servers.component.html',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = ' test server';
+  serverCreated = false;
+  servers = ['TestServer', 'TestServer2'];
+  showPara = false;
+  username = '';
+  btnClickArray: string[] = [];
+  btnClicks = 0;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus =
+      'server was created, Name is ' + this.serverName;
+  }
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  ShowPara() {
+    this.showPara = !this.showPara;
+    this.btnClicks++;
+    this.btnClickArray.push(Date.now().toString());
+  }
 }
